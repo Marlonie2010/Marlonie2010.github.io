7e1e7c3b68e683c12fb38aef123dd2ff8a7b7e80
Florian Zimmermann <zimmie79@outlook.de>
2019-02-07 10:17:28 +0100

    Add files via upload

--- js/src.js
+++ js/src.js
@@ -29,13 +29,13 @@ function main(){
 	currentG = new Array();
 	currentB = new Array();
 
-	for(var c = 0;c < 500;c++){
+	for(var c = 0;c < 1000;c++){
 		currentX[c] = ScreenWidth * 0.5;
 		currentY[c] = ScreenHeight * 0.5;
 
-		currentR[c] = Math.random() * 255;
-		currentG[c] = Math.random() * 255;
-		currentB[c] = Math.random() * 255;
+		currentR[c] = Math.floor(Math.random() * 255);
+		currentG[c] = Math.floor(Math.random() * 255);
+		currentB[c] = Math.floor(Math.random() * 255);
 
 	}
 	
@@ -70,13 +70,12 @@ function addLine1(){
 
 		ctx.moveTo(currentX[c], currentY[c]);
 		
-		ctx.strokeStyle = `rgb(
-		   ${currentR[c]},
-		   ${currentG[c]},
-		   ${currentB[c]})`;
-		
+		ctx.strokeStyle = "#" + (currentR[c]).toString(16) + (currentG[c]).toString(16) + (currentB[c]).toString(16);
+
 		ctx.lineTo(x, y); 
 		
+		//ctx.arc(x, y, 2, 0, 2*Math.PI);
+
 		ctx.stroke();
 		
 		currentX[c] = x;
